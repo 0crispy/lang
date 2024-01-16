d9621ed66d4edbfa6000432906663c62684c7fbd
0crispy <krispastas@gmail.com>
2024-01-16 22:35:58 +0200

    commit

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,14 @@
 mod tokenizer;
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Range, sync::Mutex, hash::Hash, iter::Peekable, vec::IntoIter};
 use tokenizer::{Token,TokenizerError};
+
+use self::tokenizer::{TokenInfo, Tokens};
 
 #[derive(Debug)]
 pub enum ParseError{
     Tokenizer(TokenizerError),
-    NoNameAfterFunction,
+    InvalidToken(Range<usize>, String),
+    Placeholder,
 }
 impl From<TokenizerError> for ParseError{
     fn from(error:TokenizerError) -> Self{
@@ -20,9 +23,9 @@ pub struct IntermediateCode{
 struct Function{
     scope:Scope,
     name:String,
-    input:FunctionInput,
-    output:Type,
-    statement:Statement,
+    input:Vec<FunctionInput>,
+    output:Option<Type>,
+    block:Block,
 }
 #[derive(Debug)]
 struct FunctionInput{
@@ -30,15 +33,22 @@ struct FunctionInput{
     input_type:Type,
 }
 #[derive(Debug)]
-struct Statement{
-    contents:Box<StatementContents>,
-    next:Option<Box<Statement>>,
+struct Block{
+    statements:Vec<Statement>,
+}
+impl Block{
+    fn placeholder() -> Self{
+        Self{
+            statements: Vec::new(),
+        }
+    }
 }
 #[derive(Debug)]
-enum StatementContents{
+enum Statement{
+    None,
     NewVariable{
         var:Variable,
-        value:Statement,
+        value:Block,
     }
 }
 #[derive(Debug)]
@@ -49,31 +59,146 @@ struct Variable{
 #[derive(Debug)]
 struct Scope;
 #[derive(Debug)]
-struct Type;
+enum Type{
+    TypeName(String),
+    TypeTraitName(String),
+}
 
 pub fn parse_code(code:&str) -> Result<IntermediateCode, ParseError>{
-    let mut tokens = tokenizer::tokenize(&code)?;
-    println!("{:?}", tokens);
-    
-    let mut tokens = tokens.into_iter();
-    loop{
-        let token = if let Some(token) = tokens.next() {
-            token
-        }else {break;};
-        match token{
-            Token::NewFunction => {
-                //now we expect the name of the function:
-                if let Some(Token::Name(name)) = tokens.next(){
+    let mut parser = Parser::new(code)?;
+    parser.parse()?;
 
+
+    Ok(parser.intermediate)
+} 
+struct Parser{
+    tokens:Mutex<Peekable<IntoIter<TokenInfo>>>,
+    tokens_info:Tokens,
+    intermediate:IntermediateCode,
+}
+impl Parser{
+    fn new(code:&str) -> Result<Self,ParseError>{
+        let tokens_info = tokenizer::tokenize(&code)?;
+        println!("{:?}", tokens_info.tokens);
+        let tokens = Mutex::new(tokens_info.tokens.clone().into_iter().peekable());
+        let intermediate = IntermediateCode{
+            global_functions: HashMap::new(),
+        };
+        Ok(Parser { tokens, tokens_info, intermediate })
+    }
+    fn parse(&mut self) -> Result<(),ParseError>{
+        
+        loop{
+            let token = if let Some(token) = self.tokens.lock().unwrap().next(){
+                token
+            }
+            else{
+                break;
+            };
+
+            match token.token{
+                Token::NewFunction => {
+                    let function = self.parse_function()?;
+                    self.intermediate.global_functions.insert(function.name.clone(), function);
+                },
+                _ => {}
+            }
+        }
+        Ok(())
+    }
+    fn parse_block(&self) -> Result<Block,ParseError>{
+        let token = self.next_token("Expected start of block")?;
+        let mut contents:Vec<Statement> = Vec::new();
+        match token.token{
+            Token::NewVariable => {
+                let var_name = self.parse_name("Expected variable name")?;
+                self.expect_token(Token::Eq, "Expected '='")?;
+                //TODO
+            }
+            Token::Name(name) => {
+                let token = self.next_token("Expected SOMETHING")?;
+                //maybe a function?
+                if token.token == Token::LeftParen{
+                    //indeed, a function.
+                    
                 }
-                else{
-                    return Err(ParseError::NoNameAfterFunction);
-                }
-            },
+                //TODO
+            }
             _ => {}
         }
+        Err(ParseError::Placeholder)
     }
-    Ok(IntermediateCode{
-        global_functions:HashMap::new()
-    })
-} 
+    fn parse_function(&self) -> Result<Function,ParseError>{
+        let fn_name = self.parse_name("Expected function name")?;
+        self.expect_token(Token::LeftParen, "Expected left paren")?;
+        let mut fn_arguments = Vec::new();
+        loop{
+            let token = self.next_token("Expected ')' or function argument name")?;
+            if let Token::RightParen = token.token{
+                break;
+            }
+            let var_name = if let Token::Name(var_name) = token.token{var_name}else{
+                return Err(ParseError::InvalidToken(token.char_range, "Expected ')' or function argument name".to_string()))
+            };
+            self.expect_token(Token::Colon, "Expected ':' ")?;
+            let argument_type = self.parse_type()?;
+
+            fn_arguments.push(FunctionInput { name: var_name, input_type:argument_type });
+            let token = self.next_token("Expected ')' or ','")?;
+            if token.token == Token::RightParen{
+                break;
+            }
+            if token.token == Token::Comma{
+                self.next_token("Expected function argument name")?;
+            }
+        }
+        self.expect_token(Token::LeftBraces,"Expected '{'")?;            
+        let block = self.parse_block()?;
+        self.expect_token(Token::RightBraces, "Expected '}'")?;
+        Ok(Function{
+            scope: Scope,
+            name: fn_name,
+            input: fn_arguments,
+            output: None,
+            block,
+        })
+    }
+    fn next_token(&self, unwrap_msg:&str) -> Result<TokenInfo, ParseError>{
+        if let Some(token) = self.tokens.lock().unwrap().next(){
+            Ok(token.clone())
+        }
+        else{
+            return Err(ParseError::InvalidToken(self.tokens_info.last_char.clone(),unwrap_msg.to_string()));
+        }
+    }
+    fn expect_token(&self, check_token:Token, unwrap_msg:&str) -> Result<(),ParseError>{
+        let token = self.next_token(unwrap_msg)?;
+        if token.token != check_token{
+            return Err(ParseError::InvalidToken(token.char_range, unwrap_msg.to_string()))
+        }
+        Ok(())
+    }
+    fn parse_name(&self, unwrap_msg:&str) -> Result<String,ParseError>{
+        let token = self.next_token(unwrap_msg)?;
+        if let Token::Name(name) = token.token{
+            Ok(name)
+        }
+        else{
+            Err(ParseError::InvalidToken(token.char_range, unwrap_msg.to_string()))
+        }
+    }
+    fn parse_type(&self) -> Result<Type,ParseError>{
+        let mut token = self.next_token("Expected type ")?;
+        if token.token == Token::SingleQuote{
+            token = self.next_token("Expected type ")?;
+            let var_trait_type = if let Token::Name(var_type) = token.token.clone(){var_type}
+            else{return Err(ParseError::InvalidToken(token.char_range,"Expected trait".to_string()))};
+            return Ok(Type::TypeTraitName(var_trait_type));
+        }
+        else{
+            let var_type = if let Token::Name(var_type) = token.token.clone(){var_type}
+            else{return Err(ParseError::InvalidToken(token.char_range,"Expected type ".to_string()))};
+            return Ok(Type::TypeName(var_type));
+        }
+    }
+}
--- src/parser/tokenizer.rs
+++ src/parser/tokenizer.rs
@@ -1,4 +1,16 @@
-#[derive(Debug, PartialEq)]
+use std::ops::Range;
+
+#[derive(Debug, Clone)]
+pub struct TokenInfo{
+    pub token:Token,
+    pub char_range:Range<usize>,
+}
+impl TokenInfo{
+    fn new(token:Token, char_range:Range<usize>) -> Self{
+        Self { token, char_range}
+    } 
+}
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token{
     Name(String),
 
@@ -7,6 +19,7 @@ pub enum Token{
 
     EndSentence,
     Colon,
+    Comma,
 
     LeftParen,
     RightParen,
@@ -24,14 +37,16 @@ pub enum Token{
 }
 #[derive(Debug)]
 pub struct Tokens{
-    tokens:Vec<Token>,
+    pub tokens:Vec<TokenInfo>,
     current_token:String,
+    pub last_char:Range<usize>,
 }
 impl Tokens{
     fn new() -> Self{
         Self{
             tokens:Vec::new(),
             current_token:String::new(),
+            last_char: Range::default(),
         }
     }
     fn name_to_token (token_str:String) -> Token{
@@ -41,31 +56,35 @@ impl Tokens{
             _ => Token::Name(token_str)
         }
     }
-    fn push_token_name(&mut self){
+    fn push_token_name(&mut self, char_id:usize){
         if !self.current_token.is_empty() {
-            self.tokens.push(Tokens::name_to_token(self.current_token.clone()));
+            self.tokens.push(
+                TokenInfo::new(
+                    Tokens::name_to_token(self.current_token.clone()),
+                    (char_id-self.current_token.clone().len())..char_id
+                )
+            );
             self.current_token.clear();
         }
     }
-    fn push_token(&mut self, token:Token){
-        self.push_token_name();
-        self.tokens.push(token);
+    fn push_token(&mut self, token:Token, char_id:usize){
+        self.push_token_name(char_id);
+        self.tokens.push(TokenInfo::new(token, char_id..char_id));
     }
 }
 #[derive(Debug)]
 pub enum TokenizerError{
 }
 
-pub fn tokenize(input: &str) -> Result<Vec<Token>, TokenizerError> {
+pub fn tokenize(input: &str) -> Result<Tokens, TokenizerError> {
     let mut tokens = Tokens::new();
-
-    for (_id,c) in input.chars().enumerate() {
+    for (id,c) in input.chars().enumerate() {
         match c {
             'a'..='z' | 'A'..='Z' | '0'..='9' | '_' => {
                 tokens.current_token.push(c);
             },
             ' ' => {
-                tokens.push_token_name();
+                tokens.push_token_name(id);
             },
             token => {
                 let t = match token{
@@ -79,14 +98,16 @@ pub fn tokenize(input: &str) -> Result<Vec<Token>, TokenizerError> {
                     ':' => Token::Colon,
                     ';' => Token::EndSentence,
                     '\'' => Token::SingleQuote,
+                    ',' => Token::Comma,
                     _ => Token::None,
                 };
                 if t != Token::None{
-                    tokens.push_token(t);
+                    tokens.push_token(t, id);
                 }
             },
         }
+        tokens.last_char = id..id;
     }
-    tokens.push_token_name();
-    Ok(tokens.tokens)
+    tokens.push_token_name(tokens.last_char.end);
+    Ok(tokens)
 }
